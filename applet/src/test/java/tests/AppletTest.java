103cf3008098e714a0b7e801473f5e40dd60b86c
Antonín Dufka <xdufka1@fi.muni.cz>
2021-07-04 22:47:46 +0200

    Add Ed25519 verification to test

--- applet/src/test/java/tests/AppletTest.java
+++ applet/src/test/java/tests/AppletTest.java
@@ -1,14 +1,21 @@
 package tests;
 
 import applet.Consts;
-import applet.jcmathlib;
 import cz.muni.fi.crocs.rcard.client.CardManager;
 import cz.muni.fi.crocs.rcard.client.CardType;
+import net.i2p.crypto.eddsa.EdDSAPublicKey;
+import net.i2p.crypto.eddsa.spec.EdDSANamedCurveTable;
+import net.i2p.crypto.eddsa.spec.EdDSAParameterSpec;
+import net.i2p.crypto.eddsa.spec.EdDSAPublicKeySpec;
 import org.junit.Assert;
 import org.junit.jupiter.api.*;
+import net.i2p.crypto.eddsa.EdDSAEngine;
 
 import javax.smartcardio.CommandAPDU;
 import javax.smartcardio.ResponseAPDU;
+import java.security.MessageDigest;
+import java.security.PublicKey;
+import java.security.Signature;
 
 /**
  * Example test class for the applet
@@ -39,20 +46,21 @@ public class AppletTest extends BaseTest {
     public void tearDownMethod() throws Exception {
     }
 
-    public void keygen(CardManager cm) throws Exception {
+    public byte[] keygen(CardManager cm) throws Exception {
         final CommandAPDU cmd = new CommandAPDU(Consts.CLA_ED25519, Consts.INS_KEYGEN,0, 0);
         final ResponseAPDU responseAPDU = cm.transmit(cmd);
         Assert.assertNotNull(responseAPDU);
         Assert.assertEquals(0x9000, responseAPDU.getSW());
         Assert.assertNotNull(responseAPDU.getBytes());
         Assert.assertEquals(32, responseAPDU.getData().length);
+        return responseAPDU.getData();
     }
 
     @Test
     public void keygen_and_sign() throws Exception {
         final CardManager cm = connect();
 
-        keygen(cm);
+        byte[] pubkeyBytes = keygen(cm);
 
         byte[] data = new byte[32];
         for(int i = 0; i < data.length; ++i)
@@ -64,5 +72,11 @@ public class AppletTest extends BaseTest {
         Assert.assertEquals(0x9000, responseAPDU.getSW());
         Assert.assertNotNull(responseAPDU.getBytes());
         Assert.assertEquals(32 + 32, responseAPDU.getData().length);
+        EdDSAParameterSpec spec = EdDSANamedCurveTable.getByName(EdDSANamedCurveTable.ED_25519);
+        Signature sgr = new EdDSAEngine(MessageDigest.getInstance(spec.getHashAlgorithm()));
+        PublicKey pubKey = new EdDSAPublicKey(new EdDSAPublicKeySpec(pubkeyBytes, spec));
+        sgr.initVerify(pubKey);
+        sgr.update(data);
+        Assert.assertTrue(sgr.verify(responseAPDU.getData()));
     }
 }
