beee548bca077c6358c1bd65efcbd36c91886bed
Antonín Dufka <xdufka1@fi.muni.cz>
2024-02-21 20:05:42 +0100

    Change default reader index to 0

--- applet/src/test/java/tests/BaseTest.java
+++ applet/src/test/java/tests/BaseTest.java
@@ -67,7 +67,7 @@ public class BaseTest {
         // System.setProperty("com.licel.jcardsim.randomdata.secure", "1");
 
         runCfg.setTestCardType(cardType);
-        runCfg.setTargetReaderIndex(2);
+        runCfg.setTargetReaderIndex(0);
 
         if (cardType == CardType.REMOTE){
             runCfg.setRemoteAddress("http://127.0.0.1:9901");
